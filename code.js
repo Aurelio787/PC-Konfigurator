8fe93a5356c271ffe9e50e3fff6c2a30a2b66867
Aurelio787 <aureliopini007@gmail.com>
2026-06-11 22:08:03 +0000

    123

--- code.js
+++ code.js
@@ -77,8 +77,4 @@ startButton.addEventListener("click", function() {
       BauteilBild.style.display = "block";
     }
   }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 87bcf8784130f7dcec65fa865226db6535b99a29
